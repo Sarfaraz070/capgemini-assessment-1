e463db8c70af3ca82dc1c8362377a5250a3bb4ee
Sarfaraz070 <mohdsarfaraz0703@gmail.com>
2025-01-28 21:06:47 +0530

    Update BILL SPLIT.py

--- BILL SPLIT.py
+++ BILL SPLIT.py
@@ -2,10 +2,6 @@
 Total_bill = int(input("ENTER THE TOTAL BILL: "))
 number_people = int(input("ENTER TOTAL NUMBER OF PEOPLE: "))
 any_tip = int(input("ENTER THE TIP AMOUNT YOU WANT TO GIVE: "))
-
-# Correct the calculation of the total amount to pay
 Amount_to_pay = Total_bill + any_tip
-
-# Calculate how much each person should pay
 amount_per_person = Amount_to_pay / number_people
 print(f"Each person has to pay: {int(amount_per_person)}")
